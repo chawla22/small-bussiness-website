6d602ca55f955e278cb6642c3af657c3a57c3cc0
chawla22 <chawla22@usf.edu>
2025-07-06 19:01:55 +0530

    added functionality to the page in cc_7.js to update the headline

--- cc_7.js
+++ cc_7.js
@@ -2,3 +2,18 @@
 document.getElementById("cta-btn").addEventListener("click", () => {
   document.getElementById("services").scrollIntoView({ behavior: "smooth" });
 });
+
+
+// CTA headline update
+document.getElementById("update-button").addEventListener("click", function () {
+  const newText = document.getElementById("headline-input").value.trim();
+  console.log("New headline text:", newText); 
+  const headline = document.getElementById("cta-headline");
+  console.log("Current headline text:", headline.textContent); 
+
+  if (newText !== "") {
+    headline.textContent = newText;
+  } else {
+    alert("Please enter a valid headline.");
+  }
+});
